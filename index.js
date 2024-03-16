c814f425603b181bdd5b5f44991c31e8c59bdbee
radmauld <radityamaulidann@gmail.com>
2024-03-16 22:06:34 +0700

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-const express = require("express");
+import express from "express";
+import router from "./routers/Routes.js";
+import dotenv from "dotenv";
+import session from "express-session";
+import sequelizeStore from "connect-session-sequelize";
+import db from "./config/database.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+dotenv.config();
+
+const sessionStore = sequelizeStore(session.Store);
+const store = new sessionStore({
+  db: db,
+  checkExpirationInterval: 1 * 60 * 1000,
+  expiration: 1 * 60 * 60 * 1000,
+});
+
 const app = express();
-const port = 3000;
-var pool = require("./queries.js");
+const port = process.env.PORT;
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
+//middleware
+app.use(
+  session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+    cookie: {
+      secure: "auto",
+    },
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/image", express.static(path.join(__dirname, "uploads")));
+app.use(router);
+app.use(errorHandler);
 
-// pool.connect((err, res) => {
-//     console.log(err)
-//     console.log('connected')
-// });
-
-app.listen(3000);
+app.listen(port, console.log("Server started http://localhost:" + port));
